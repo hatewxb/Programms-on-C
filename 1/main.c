481bc25cf7df8a19bd362e13a1894a0e1f305b82
lifeisjustfuckingmistake <wxbwxbwxb69@gmail.com>
2023-02-13 20:06:06 +1000

    Lab 1

---
+++ 1/main.c
@@ -0,0 +1,27 @@
+#include "stdio.h"
+#include "math.h"
+#include "stdlib.h"
+
+int main(int argc, char *arvg[])
+{
+    int a, b, c, d;
+    double x1,x2;
+    printf("Enter your mutable values:\n");
+    scanf("%d %d %d", &a, &b, &c);
+    d=b*b-4*a*c;
+    printf("D=%d \n", d);
+    if (d<0)
+    {
+        printf("Roots don't exist");
+        return 0;
+    }
+    if (d==0)
+    {
+        x1=-(b/(2*a));
+        printf("x1=%lf", x1);
+        return 0;
+    }
+    x1 = (-b + sqrt(d)) / (2*a);
+    x2 = (-b - sqrt(d)) / (2*a);
+    printf("x1=%lf, x2=%lf",x1,x2);
+}   
